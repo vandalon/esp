a130c5e7da5186b01244265d26f632bbadd55348
Joris Vandalon <joris@vandalon.nl>
2017-04-20 22:24:27 +0200

    badkamer2 project. Relays + PIR

---
+++ badkamer2/init.lua
@@ -0,0 +1,27 @@
+local config=require('config')
+
+local telnet=require('telnet_srv')
+telnet.setupTelnetServer()
+
+local function startup()
+    local main=require('main')
+    main.mqtt_connect()
+end
+
+wifi.setmode(wifi.STATION)
+print('set mode=STATION (mode='..wifi.getmode()..')')
+print('MAC: ',wifi.sta.getmac())
+print('chip: ',node.chipid())
+print('heap: ',node.heap())
+wifi.sta.config(config.ssid,config.wifi_psk)
+wifi.sta.setip({ip=config.ip,netmask=config.netmask,gateway=config.gateway})
+wifi.sta.eventMonReg(wifi.STA_WRONGPWD, function() node.restart() end)
+wifi.sta.eventMonReg(wifi.STA_APNOTFOUND, function() node.restart() end)
+wifi.sta.eventMonReg(wifi.STA_FAIL, function() node.restart() end)
+wifi.sta.eventMonReg(wifi.STA_GOTIP, function()
+    print("Network connected.")
+    print("Waiting 5 seconds before starting main script.")
+    tmr.alarm(0,5000,0,startup)
+end)
+wifi.sta.eventMonStart()
+wifi.sta.connect()
